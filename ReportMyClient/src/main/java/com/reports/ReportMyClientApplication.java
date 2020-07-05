c8c6084955930ea0ecf8ad9b358a89603e710732
stasfainberg <stas.fainberg@gmail.com>
2020-07-05 17:03:22 +0300

    initial

---
+++ ReportMyClient/src/main/java/com/reports/ReportMyClientApplication.java
@@ -0,0 +1,13 @@
+package com.reports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportMyClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportMyClientApplication.class, args);
+	}
+
+}
